05f0eae78796a1840a512ff8d5f40dabb1396361
Venkata Vinay Vijjapu <102150068+venkatavinayvijjapu@users.noreply.github.com>
2023-09-28 08:06:36 +0530

    Update app.py

--- chatbot-llama/app.py
+++ chatbot-llama/app.py
@@ -4,20 +4,11 @@ import os
 
 # App title
 st.set_page_config(page_title="")
-replicate_api='r8_K0JYx41DklqijcCjIq8VxU9eWHZwGgM1pktu1'
+replicate_api=Your Api key
 # Replicate Credentials
 with st.sidebar:
     st.title('Chat Assistant by llama 💬')
-    # if 'REPLICATE_API_TOKEN' in st.secrets:
-    #     st.success('API key already provided!', icon='✅')
-    #     replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    # else:
-    #     replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    #     if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-    #         st.warning('Please enter your credentials!', icon='⚠️')
-    #     else:
-    #         st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = 'r8_K0JYx41DklqijcCjIq8VxU9eWHZwGgM1pktu1'
+    os.environ['REPLICATE_API_TOKEN'] = Your API Key
 
     st.subheader('Models and parameters')
     selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
